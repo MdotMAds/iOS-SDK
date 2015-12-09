679024781c414d0b853c47fe9ee4d3c6389de4f9
Avinash Nityananda <avinash@techjini.com>
2015-12-09 17:28:32 +0530

    added sample app for 3.6.2

---
+++ MdotM-iOS-SampleApp-3.6.1/MdotMSampleApp/MdotMSampleApp/MdotM files/MdotMNativeView.h
@@ -0,0 +1,30 @@
+//
+//  MdotMNativeView.h
+//  MdotMAdsSDK2
+//
+//  Created by TechJini on 12/08/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "MdotMRequestParameters.h"
+#import "MdotMNativeDelegate.h"
+
+@interface MdotMNativeView : UIView< UIGestureRecognizerDelegate>  {
+    id<MdotMNativeDelegate> nativeDelegate;
+    MdotMRequestParameters* requestObject;
+}
+@property (nonatomic,assign) BOOL isRedirecting;
+
+@property (nonatomic,assign) id<MdotMNativeDelegate> nativeDelegate;
+@property (nonatomic, retain) MdotMRequestParameters* requestObject;
+@property (nonatomic,assign) BOOL requestingNativeAd;
+
+
+-(void)registerView:(UIView*)nativeAdView withNativeID:(NSString*)nativeID withViewController:(UIViewController*)viewController;
+-(void)unregisterView:(UIView*)nativeAdView;
+-(BOOL)loadNativeAd:(MdotMRequestParameters *)request;
+
+
+
+@end

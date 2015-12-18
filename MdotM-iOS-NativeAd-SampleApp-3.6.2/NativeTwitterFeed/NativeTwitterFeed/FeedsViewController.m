ab475e619b31845e288cbf68eaaada2dbc8db8b3
Avinash Nityananda <avinash@techjini.com>
2015-12-18 19:24:33 +0530

    3.6.2 NativeAd-TwitterFeed-SampleApp

--- MdotM-iOS-NativeAd-SampleApp-3.6.2/NativeTwitterFeed/NativeTwitterFeed/FeedsViewController.m
+++ MdotM-iOS-NativeAd-SampleApp-3.6.2/NativeTwitterFeed/NativeTwitterFeed/FeedsViewController.m
@@ -18,7 +18,7 @@
 
 
 @interface FeedsViewController (){
-    }
+    } 
 @property (nonatomic, strong) MdotMRequestParameters *intRequest;
 
 @property(nonatomic,strong) NSMutableArray *twitterFeedsArray ;
